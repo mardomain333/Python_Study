0c2abc1e6b81fec2243032c01d4ed43ad5205cbf
Gokul <118746900+mardomain333@users.noreply.github.com>
2025-12-16 23:58:21 +0530

    16/12/25

--- pattern_printing.py
+++ pattern_printing.py
@@ -1,6 +1,20 @@
+"""
+1                1
+
+1 2            2 1
+
+1 2 3        3 2 1
+
+1 2 3 4    4 3 2 1
+
+1 2 3 4  5 4 3 2 1
+
+done by myown logic
+"""
+
+
+
 n=5
-
-
 max=2*n-1
 for i in range(1,n+1):
     if i!=n:
@@ -22,4 +36,39 @@ for i in range(1,n+1):
         print(n,end="")
         print(string[::-1])
   
+
+
+
+"""
+
+           1     
+         1 2 1    
+       1 2 3 2 1
+     1 2 3 4 3 2 1
+   1 2 3 4 5 4 3 2 1
+   done by myown login proud :>>>
+"""
+n=int(input("Enter the number"))
+n=n+1
+for i in range(1,n):
+        string=""
+        count=0                   #this my idea for printing the space before the number if i=1 that means (n-1) (5-1) 4 spaces
+                                   #are requried
+        string+="  "*(n-i)
+        m=i+(i-1)
+
+
+        for j in range(1,m+1):
+                if j<=(m//2)+1:
+                        count+=1
+                        string+=" "+str(count)       #this is the logic for printing the number pattern
+                        
+                else:
+                        count-=1
+                        string+=" "+str(count)
+        
+                
+        string+="  "*(n-i)
+        print(string)
+                
 
