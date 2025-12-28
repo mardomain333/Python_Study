b4261d92126032fa269d29aed9a6f0c3411ea687
Gokul <118746900+mardomain333@users.noreply.github.com>
2025-12-28 22:20:38 +0530

    Two sum added

--- Twosum.py
+++ Twosum.py
@@ -1,23 +1,12 @@
-l=[9,4,20,3,10,5,-1]      #maximum subarray sum equals target
-l=[5,2,2,1,1,1,1,4]
-target=4
+l=[1,2,2,3,4,5]
+target=5
 
-hastmap={}
-count=-1
-prefixsum=0
-hastmap[0]=count
-maxvalue=99
-coco=0
-for a in l:
-    count+=1
-    prefixsum+=a
-    dono=prefixsum-target
-    if dono in hastmap.keys():
-          maxvalue=count-hastmap[dono] if maxvalue> (count-hastmap[dono]) else maxvalue
-          coco+=1
-          print(l[hastmap[dono]+1:count+1])
-    hastmap[prefixsum]=count
-print(hastmap)
-print(maxvalue)
+serve={
 
+}
+for i in range(len(l)):
+    if target-l[i]in serve.keys():
+        print(serve[target-l[i]],i)
+    else:
+        serve[l[i]]=i
 
